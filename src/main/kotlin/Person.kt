dcc3c4d9abb43640f7a80cc37a6f3532bcb43c4e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-20 12:34:19 +0200

    add var validInput

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,21 +1,23 @@
+open class Person(id: String, name: String, pass: String) {
 
-open class Person(id:String, name: String, pass: String) {
+    fun printPerson() {
+        var validInput = false
+        while (!validInput) {
+            println("Sind Sie ein Kunde oder ein Admin? (Geben Sie Kunde/Admin ein)")
+            val userType = readLine()
+            val admin = mutableListOf<Product>()
 
-    fun printPerson(){
-        println("Sind Sie ein Kunde oder ein Admin? (Geben Sie Kunde/Admin ein)")
-        val userType = readln()
-        val admin = mutableListOf<Product>()
-
-        if (userType == "Kunde" || userType == "kunde") {
-            val kunde = Kunde("Kunde", "pass")
-            kunde.login()
-        } else if (userType == "Admin" || userType == "admin") {
-            val admin = Admin("Admin", "pass")
-            admin.login()
-        } else {
-            println("Ungültige Auswahl.")
+            if (userType == "Kunde" || userType == "kunde") {
+                val kunde = Kunde("Kunde", "pass")
+                kunde.login()
+                validInput = true
+            } else if (userType == "Admin" || userType == "admin") {
+                val admin = Admin("Admin", "pass")
+                admin.login()
+                validInput = true
+            } else {
+                println("Ungültige Auswahl. Bitte versuchen Sie es erneut.")
+            }
         }
-
     }
-
 }

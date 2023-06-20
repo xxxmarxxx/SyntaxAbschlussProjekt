414294b0ab0d3301222723cae2c5ab992a1ac01f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-20 18:49:27 +0200

    add kunde if

--- src/main/kotlin/Login.kt
+++ src/main/kotlin/Login.kt
@@ -30,10 +30,26 @@ class Login {
         var beenden = false
         while (!beenden) {
             println("Bitte wählen Sie eine Option: \n1. Kunst von Künstler kaufen \n2. Kunst von Künstler aus Warenkorb löschen \n3. Meine Warenkorb \n4. Beenden")
-            when (readLine()) {
-                "1" -> println("Kunst von Künstler kaufen")
-                "2" -> println("Kunst von Künstler aus Warenkorb löschen")
-                "3" -> println("Meine Warenkorb")
+            when (readln()) {
+                "1" -> {
+                    println("Kunst von Künstler kaufen")
+                    val warenProdukt = readln()
+                    warenkorb.add(warenProdukt)
+                    println("Produkt '$warenProdukt' wurde hinzugefügt.")
+                }
+                "2" -> {
+                    println("Kunst von Künstler aus Warenkorb löschen")
+                    val loeschenProdukt = readln()
+                    if (warenkorb.contains(loeschenProdukt) {
+                        warenkorb.remove(loeschenProdukt)
+                        println("Produkt '$loeschenProdukt' wurde gelöscht.")
+                    } else {
+                        println("Das Produkt konnte nicht gefunden werden.")
+                    }
+                }
+                "3" -> {
+                    println("Meine Warenkorb mit: $warenkorb")
+                }
                 "4" -> beenden = true
                 else -> println("Ungültige Auswahl. Bitte versuchen Sie es erneut.")
             }
@@ -44,7 +60,7 @@ class Login {
         var beenden = false
         while (!beenden) {
             println("Bitte wählen Sie eine Option: \n1. Neue Kunst hinzufügen \n2. Kunst löschen \n3. Alle Bestellungen anzeigen \n4. Beenden")
-            when (readLine()) {
+            when (readln()) {
                 "1" -> {
                     println("Neue Kunst hinzufügen")
                     val neueProdukt = readln()
@@ -64,7 +80,7 @@ class Login {
                 }
                 "3" -> {
                     println("Alle Bestellungen anzeigen")
-                    println("Alle produkte: $produkt")
+                    println("Alle Künstler: $produkt")
                 }
 
                 "4" -> beenden = true

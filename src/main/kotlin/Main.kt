287e57f2df8a5ef3be65a758e9fa0b78f100ef14
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-20 12:05:33 +0200

    change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,56 @@ import onbording.onBoarding
 fun main() {
 
 //    ---------- START-logo
-    changeConsoleColor("\u001B[32m")
-    onBoarding()
-    resetConsoleColor()
-    println()
+//    changeConsoleColor("\u001B[32m")
+//    onBoarding()
+//    resetConsoleColor()
+//    println()
 //    ---------- LogoEND ----
 
 //    ----- START LOGIN KUNDE/ADMIN
 
-    Person("1","Admin","pass").printPerson()
+//    Person("1","Admin","pass").printPerson()
 
 //    ----- END LOGIN
 
+//    ------ menu Start
+    println("Gib name Admin oder Kunde ein")
     Menu().menu()
-    Menu().printMenu(UserType.ADMIN)
+//    Menu().printMenu(UserType.ADMIN)
+//    ------ END
 
+//    categoryArts("malerei")
+//    Thread.sleep(2000)
+//    fun productadapter
+//    val products = listOf(
+//        product1,
+//        product2,
+//        product3,
+//        product4,
+//        product5,
+//        product6,
+//        product7,
+//        product8,
+//        product9,
+//        product10,
+//        product11,
+//        product12,
+//        product13,
+//        product14,
+//        product15,
+//        product16,
+//        product17,
+//        product18,
+//        product19,
+//        product20,
+//        product21,
+//        product22,
+//        product23,
+//    )
+//    val productAdapter = ProductAdapter(products)
+//    productAdapter.displayProducts()
+//    println(products[0])
+//    println(products[1])
+//    println(products[2])
 
 }

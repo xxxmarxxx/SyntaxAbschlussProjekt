58bb60849abaa56dee46d4c7cd941320a7761c2f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-20 15:06:10 +0200

    add adapter

--- src/main/kotlin/ProductAdapter.kt
+++ src/main/kotlin/ProductAdapter.kt
@@ -1,16 +1,20 @@
-//class ProductAdapter(private val products: List<Product>) {
-//
-//    fun displayProducts() {
-//        for (product in products) {
-//            println("--------------------------------")
-//            println(product)
-//        }
-//    }
-//}
-//fun printProduct() {
-//    println("  Product Id ist: ${product1.id}")
-//    println("  Product befindet sich in der Kategorie: ${product1.categories}")
-//    println("  Name der Artist ist: ${product1.artist}")
-//    println("  Title des Bildes ist: ${product1.title}")
-//
-//}
+class ProductAdapter(private val products: List<Product>) {
+
+    fun displayProducts() {
+        for (product in products) {
+            println("--------------------------------")
+            println(product)
+        }
+    }
+}
+fun printProduct() {
+    println("  Product Id ist: ${Shop().product1.id}")
+    println("  Product befindet sich in der Kategorie: ${Shop().product1.categories}")
+    println("  Name der Artist ist: ${Shop().product1.artist}")
+    println("  Title des Bildes ist: ${Shop().product1.title}")
+
+}
+
+fun main(){
+    printProduct()
+}

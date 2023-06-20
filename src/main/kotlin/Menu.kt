17210d36bdbe5f8cda03159ca7af607825ce5194
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-20 09:19:00 +0200

    change menu file

--- src/main/kotlin/Menu.kt
+++ src/main/kotlin/Menu.kt
@@ -1,45 +1,52 @@
-//enum class UserType {
-//    KUNDE,
-//    ADMIN
-//}
 class Menu {
 
+    var istEsEinKundeOderEinAdmin: UserType = UserType.KUNDE
+    var beenden = false
     fun menu() {
-        var isItAKundeOrAnAdmin: UserType = UserType.KUNDE
-        while (true) {
 
-            var inputLogin = readln()
+        while (!beenden) {
+
+            val inputLogin = readln()
             if (inputLogin == "Admin") {
-                isItAKundeOrAnAdmin = UserType.ADMIN
+                istEsEinKundeOderEinAdmin = UserType.ADMIN
             } else {
-                isItAKundeOrAnAdmin = UserType.KUNDE
+                istEsEinKundeOderEinAdmin = UserType.KUNDE
             }
-            printMenu(isItAKundeOrAnAdmin)
+            printMenu(istEsEinKundeOderEinAdmin)
         }
     }
 
     fun printMenu(type: UserType) {
         if (type == UserType.ADMIN) {
-            println("1 für Neues Produkt")
-            println("2 für Produktänderung")
-            println("3 für Löschen ein Produkt")
+            println("5 für Neues Produkt")
+            println("6 für Produktänderung")
+            println("7 für Löschen ein Produkt")
+            println("8 Beenden")
 
-            when (readln()) {
-                "1" -> println("Jetzt neues Produkt hinzufügen...")
-                "2" -> println("Jetzt Produkt ändern")
-                "3" -> println("Jetzt Produkt löschen")
+
+            val adminInput = readln()
+
+            when (adminInput) {
+                "5" -> println("Jetzt neues Produkt hinzufügen...")
+                "6" -> println("Jetzt Produkt ändern")
+                "7" -> println("Jetzt Produkt löschen")
+                "8" -> beenden = true
             }
 
-        } else if (type == UserType.KUNDE) {
-            println("1 - für Produkte anzeigen")
-            println("2 - für Produkt kaufen")
-            println("3 - für warenkorb")
+        } else {
+            println("1 - Künstler Produkt kaufen")
+            println("2 - Künstler Produkt von Warenkorb löschen")
+            println("3 - Meine warenkorb anzeigen")
+            println("4 - Beenden")
 
             when (readln()) {
-                "1" -> println("Kunst von Artist zeigen")
-                "2" -> println("Kunst von Artist kaufen")
-                "3" -> println("Kunst von warenkorb löschen")
+                "1" -> println("Kunst von Künstler kaufen")
+                "2" -> println("Kunst von Künstler aus Warenkorb löschen")
+                "3" -> println("Meine warenkorb ")
+                "4" -> beenden = true
+
             }
         }
+
     }
 }

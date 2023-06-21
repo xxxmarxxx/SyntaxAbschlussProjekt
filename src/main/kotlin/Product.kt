e135997638eb3691b913666516d2eb88583b1f7b
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-21 21:08:06 +0200

    add new design to func printProduct

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -11,22 +11,42 @@ data class Product(
     val edition: Int,
 ) {
 
-    fun printProduct() {
-        changeConsoleColor("\u001B[36m")
-        println("\t****************************************")
-        println("\t▓▓\tId: $id")
-        println("\t▓▓\tKategorie: $categories")
-        println("\t▓▓\tKünstler: $artist")
-        println("\t▓▓\tTitel: $title")
-        println("\t▓▓\tDatiert: $dateOfWork")
-        println("\t▓▓\tBreite: $width cm")
-        println("\t▓▓\tHöhe: $height cm")
-        println("\t▓▓\tBeschreibung: $content")
-        println("\t▓▓\tPreis: $price €")
-        println("\t▓▓\tAuflage: $edition Stück")
-        println("\t****************************************")
-        resetConsoleColor()
-    }
+//    fun printProduct() {
+//        changeConsoleColor("\u001B[36m")
+//        println("\t****************************************")
+//        println("\t▓▓\tId: $id")
+//        println("\t▓▓\tKategorie: $categories")
+//        println("\t▓▓\tKünstler: $artist")
+//        println("\t▓▓\tTitel: $title")
+//        println("\t▓▓\tDatiert: $dateOfWork")
+//        println("\t▓▓\tBreite: $width cm")
+//        println("\t▓▓\tHöhe: $height cm")
+//        println("\t▓▓\tBeschreibung: $content")
+//        println("\t▓▓\tPreis: $price €")
+//        println("\t▓▓\tAuflage: $edition Stück")
+//        println("\t****************************************")
+//        resetConsoleColor()
+//    }
 
+    fun printProduct() {
+println()
+        changeConsoleColor("\u001B[38;5;240m")
+println("\t●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●〕〔●")
+        resetConsoleColor()
+        changeConsoleColor("\u001B[38;5;245m")
+println("\tappART ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Künstler ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ GALERIE")
+println("\t▓▓▓▓▓▓▓▓▓▓\tId: $id \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tKategorie: $categories \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓artist▓▓\tKünstler: $artist \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tTitel: $title \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓title▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tDatiert: $dateOfWork \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tBreite: $width cm \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tHöhe: $height cm \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tBeschreibung: $content \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tPreis: $price € \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓price▓▓▓▓▓▓▓▓")
+println("\t▓▓▓▓▓▓▓▓▓▓\tAuflage: $edition Stück \t▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+        resetConsoleColor()
+    Thread.sleep(500)
+    }
 }
 

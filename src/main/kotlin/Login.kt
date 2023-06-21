17810059c223b162ba6642da7909f5462abcbb82
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-22 00:59:42 +0200

    change fun showCustomerMenu add mutableList, val warenProduktId, add val produktZumHinzufuegen und if , add for in warenkorb

--- src/main/kotlin/Login.kt
+++ src/main/kotlin/Login.kt
@@ -1,18 +1,19 @@
 import java.util.*
 
 class Login {
+//    Login Admin/Kunde mit valid
     fun loginIn() {
         var validInput = false
         while (!validInput) {
             println("\tSind Sie ein Kunde oder ein Admin? (Geben Sie Kunde/Admin ein)")
-            val userType = readLine()
+            val userType = readln()
             val admin = mutableListOf<Product>()
 
             if (userType == "Kunde" || userType == "kunde") {
                 val kunde = Kunde("Kunde", "pass")
                 kunde.login()
                 validInput = true
-                showCustomerMenu()
+                showCustomerMenu(shop = Shop())
             } else if (userType == "Admin" || userType == "admin") {
                 val admin = Admin("Admin", "pass")
                 admin.login()
@@ -24,10 +25,54 @@ class Login {
         }
     }
 
-    val shop = Shop()
-    val produkt = mutableListOf<String>()
-    val warenkorb = mutableListOf<String>()
-    fun showCustomerMenu() {
+//    func Kunde Menu
+//    val shop = Shop()
+//    val produkt = mutableListOf<String>()
+//    val warenkorb = mutableListOf<Product>()
+//    fun showCustomerMenu() {
+//        var beenden = false
+//        while (!beenden) {
+//            println("\tBitte wählen Sie eine Option: \n\t1. Kunst von Künstler kaufen \n\t2. Kunst von Künstler aus Warenkorb löschen \n\t3. Meine Warenkorb \n\t4. Beenden")
+//            abstandUnten(3)
+//            when (readln()) {
+//                "1" -> {
+//                    println("\tKunst von Künstler kaufen")
+//                    println("\tWir haben Kunst dieser Künstler im Angebot: ")
+//                    for (product in shop.productsList){
+//                        product.printProduct()
+//
+//                    }
+//                    val warenProdukt = readln()
+//                    warenkorb.add(warenProdukt)
+//                    println("\tProdukt '$warenProdukt' wurde hinzugefügt.")
+//                }
+//
+//                "2" -> {
+//                    println("\tKunst von Künstler aus Warenkorb löschen")
+//                    val loeschenProdukt = readln()
+//                    if (warenkorb.contains(loeschenProdukt)) {
+//                        warenkorb.remove(loeschenProdukt)
+//                        println("\tProdukt '$loeschenProdukt' wurde gelöscht.")
+//                    } else {
+//                        println("\tDas Produkt konnte nicht gefunden werden.")
+//                    }
+//                }
+//
+//                "3" -> {
+//                    println("\tMeine Warenkorb mit: $warenkorb")
+//                }
+//
+//                "4" -> beenden = true
+//                else -> println("\tUngültige Auswahl. Bitte versuchen Sie es erneut.")
+//            }
+//        }
+//    abstandUnten(3)
+//    }
+
+
+
+    fun showCustomerMenu(shop: Shop) { // Passing Shop object
+        val warenkorb = mutableListOf<Product>()
         var beenden = false
         while (!beenden) {
             println("\tBitte wählen Sie eine Option: \n\t1. Kunst von Künstler kaufen \n\t2. Kunst von Künstler aus Warenkorb löschen \n\t3. Meine Warenkorb \n\t4. Beenden")
@@ -38,24 +83,42 @@ class Login {
                     for (product in shop.productsList){
                         product.printProduct()
                     }
-                    val warenProdukt = readln()
-                    warenkorb.add(warenProdukt)
-                    println("\tProdukt '$warenProdukt' wurde hinzugefügt.")
+                    val warenProduktId = readln().toIntOrNull()
+                    if (warenProduktId != null) {
+                        val produktZumHinzufuegen = shop.productsList.find { it.id == warenProduktId }
+                        if (produktZumHinzufuegen != null) {
+                            warenkorb.add(produktZumHinzufuegen)
+                            shop.productsList.remove(produktZumHinzufuegen) // kunstler Produkt aus dem datenBank löschen
+                            println("\tProdukt mit Id '$warenProduktId' wurde hinzugefügt.")
+                        } else {
+                            println("\tProdukt mit Id '$warenProduktId' wurde nicht gefunden.")
+                        }
+                    } else {
+                        println("\tUngültige Eingabe. Bitte versuchen Sie es erneut.")
+                    }
                 }
 
                 "2" -> {
                     println("\tKunst von Künstler aus Warenkorb löschen")
-                    val loeschenProdukt = readln()
-                    if (warenkorb.contains(loeschenProdukt)) {
-                        warenkorb.remove(loeschenProdukt)
-                        println("\tProdukt '$loeschenProdukt' wurde gelöscht.")
+                    val loeschenProduktId = readln().toIntOrNull()
+                    if (loeschenProduktId != null) {
+                        val produktZumLoeschen = warenkorb.find { it.id == loeschenProduktId }
+                        if (produktZumLoeschen != null) {
+                            warenkorb.remove(produktZumLoeschen)
+                            println("\tProdukt mit Id '$loeschenProduktId' wurde gelöscht.")
+                        } else {
+                            println("\tDas Produkt konnte nicht gefunden werden.")
+                        }
                     } else {
-                        println("\tDas Produkt konnte nicht gefunden werden.")
+                        println("\tUngültige Eingabe. Bitte versuchen Sie es erneut.")
                     }
                 }
 
                 "3" -> {
-                    println("\tMeine Warenkorb mit: $warenkorb")
+                    println("\tMeine Warenkorb mit: ")
+                    for (product in warenkorb) {
+                        product.printProduct()
+                    }
                 }
 
                 "4" -> beenden = true
@@ -64,7 +127,13 @@ class Login {
         }
     }
 
+
+
+
+
+    //    func Admin Menu
     fun showAdminMenu() {
+        val produkt = mutableListOf<String>()
         var beenden = false
         while (!beenden) {
             println("\tBitte wählen Sie eine Option: \n\t1. Neue Kunst hinzufügen \n\t2. Kunst löschen \n\t3. Alle Bestellungen anzeigen \n\t4. Beenden")
@@ -96,6 +165,7 @@ class Login {
                 else -> println("\tUngültige Auswahl. Bitte versuchen Sie es erneut.")
             }
         }
+        abstandUnten(3)
     }
 
 

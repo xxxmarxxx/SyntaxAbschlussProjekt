1eef6604a28109143a8b0799088d2a4c8c4cdc76
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-21 15:13:35 +0200

    testing

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,6 +1,10 @@
-fun main(){
+fun main() {
 //    println(Shop().categoryArts("Acryl"))
 //    println("das bla ${Shop().categoryArts("NFT")} das ist das ")
 
-Shop().suchenArtInKategorie()
+//Shop().suchenArtInKategorie()
+//Shop().suchenArtInShop()
+
+    val shopTest = Shop()
+    shopTest.suchenArtInShop()
 }

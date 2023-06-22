8a8c8180caa796dc0a3d754f3b4e0abb3a2bd9c1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-22 17:11:46 +0200

    add tab

--- src/main/kotlin/Admin.kt
+++ src/main/kotlin/Admin.kt
@@ -4,18 +4,18 @@ class Admin(username: String, password: String) : User(username, password, UserT
 
     fun login() {
         while (true) {
-            println("Bitte geben Sie Ihren Benutzernamen ein:")
+            println("\tBitte geben Sie Ihren Benutzernamen ein:")
             val inputUsername = readln() ?: ""
 
-            println("Bitte geben Sie Ihr Passwort ein:")
+            println("\tBitte geben Sie Ihr Passwort ein:")
             val inputPassword = readln() ?: ""
 
             if (anmeldeTest(inputUsername, inputPassword)) {
                 // Wenn der Login erfolgreich ist, greifen Sie auf Admin-spezifische Funktionen zu
-                println("$userName hat sich erfolgreich eingeloggt \nund greift nun auf Admin-Funktionen zu.")
+                println("\t$userName hat sich erfolgreich eingeloggt \nund greift nun auf Admin-Funktionen zu.")
                 break
             } else {
-                println("Login fehlgeschlagen. Bitte versuchen Sie es erneut.")
+                println("\tLogin fehlgeschlagen. Bitte versuchen Sie es erneut.")
             }
         }
 

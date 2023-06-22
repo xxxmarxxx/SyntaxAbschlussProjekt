ab1ee6dd4b3f415a4a743b9891a022dbe43b8af6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-06-22 11:37:08 +0200

    add new tests

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,10 +1,30 @@
 fun main() {
 //    println(Shop().categoryArts("Acryl"))
 //    println("das bla ${Shop().categoryArts("NFT")} das ist das ")
+//
+//    Shop().suchenArtInKategorie()
+//    Shop().suchenArtInShop()
+//
+//    val shopTest = Shop()
+//    shopTest.suchenArtInShop()
+//
+//    leerZeichen("test")
+//    leerZeichen2("test")
+//    colorGrafik()
+    escapedCharakters()
+    println("sdsdsdsdsd")
+    leerZeichen2("20")
+    dotLine(10,"3 ")
 
-//Shop().suchenArtInKategorie()
-//Shop().suchenArtInShop()
+    // Print some text
+    println("Hello, World!")
 
-    val shopTest = Shop()
-    shopTest.suchenArtInShop()
+    // Add some blank lines
+    for (i in 15 downTo 1) {
+        println()
+    }
+
+    // Print some more text
+    println("This text is separated from the previous text by blank lines.")
+
 }
